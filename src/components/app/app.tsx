60001a2b947e950161abf513c7bb53aec4919011
Maksim <70702008m@gmail.com>
2026-04-23 21:34:04 +0300

    tytyty

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,27 +1,27 @@
 
 
 export default function App() {
-  const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  event. preventDefault();
     const form = event.currentTarget;
     const formData = new FormData(form);
-    const username = formData.get("username") as string;
+    const username = formData.get("user") as string;
     if (!username.trim()) {
-      console.log(alert("Заповни поле"));
+     alert("Заповни поле");
       return;
     }
     console.log(username);
-   }
+    form.reset();
 
+}
+  
   return (
-    <>
-      <h1>Form</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Name</label>
-      <input type="text" name="username" id="username" />
+    <> <h1>Form</h1>
+    <form onSubmit={handleSubmit}>
+        <label htmlFor="username">User</label>
+        <input type="text" name="user" id="username" />
         <button type="submit">Submit</button>
-      </form>
-     
-    </>
+    </form></>
+  
   )
 }
