35e5cc4cecbdc627d8058c1543e6c900a58f23f5
Maksim <70702008m@gmail.com>
2026-04-24 19:50:41 +0300

    text

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,27 +1,12 @@
-
+import Form from "../Forms/form"
 
 export default function App() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-  event. preventDefault();
-    const form = event.currentTarget;
-    const formData = new FormData(form);
-    const username = formData.get("user") as string;
-    if (!username.trim()) {
-     alert("Заповни поле");
-      return;
-    }
-    console.log(username);
-    form.reset();
 
-}
   
   return (
-    <> <h1>Form</h1>
-    <form onSubmit={handleSubmit}>
-        <label htmlFor="username">User</label>
-        <input type="text" name="user" id="username" />
-        <button type="submit">Submit</button>
-    </form></>
+    <>
+      <Form />
+    </>
   
   )
 }
